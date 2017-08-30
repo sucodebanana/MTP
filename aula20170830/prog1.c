e960deffadb0a39b5b7b0a163e8725b2442fc75f
sucodebanana <sucodebanana@msn.com>
2017-08-30 10:44:26 -0300

    fim de aula

---
+++ aula20170830/prog1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int conv(char * str)
+{
+	int x=0;
+	int i=0;
+	while(str[i]) {
+		x=x*10+str[i]-48;i++;
+		}
+	return x;
+}
+
+int main(int argc, char ** argv)
+{
+	if(argc>=3){
+
+	printf("Soma = %d\n",
+		conv(argv[1])+conv(argv[2]));
+	printf("Sub = %d\n",
+		conv(argv[1])-conv(argv[2]));
+	printf("Resto = %d\n",
+		conv(argv[1])%conv(argv[2]));
+	}
+	else
+		printf("Falta argurmentos!\n");
+
+	return 0;
+}
---
+++ aula20170830/teste.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Estou vivo!\n");
+
+	return 0;
+}
